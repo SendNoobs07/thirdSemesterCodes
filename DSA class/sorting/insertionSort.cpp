378512f22137fb9f645ed7be3a8e6b696ac993c4
d33panpaudel07 <141331201+d33panpaudel07@users.noreply.github.com>
2024-03-27 12:52:47 +0545

    Update insertionSort.cpp

--- DSA class/sorting/insertionSort.cpp
+++ DSA class/sorting/insertionSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-// THE CODE IS NOT COMPLETED
-class Test
+
+class InsertionSort
 {
 private:
     int demo[10] = {2, 5, 3, 1, 8, 93, 7, 55, 11, 0};
@@ -14,19 +14,49 @@ public:
         y = temp;
     }
 
-    void mainProcess(){
-        exchange(demo[1],demo[4]);
-
-        for(int i=0;i<10;i++)
+    void sortLeft(int &x, int &y, int i)
+    {
+        if ((i != 0) && x > y)
         {
-            cout<<demo[i]<<", ";
+            cout << x << " and " << y << " were exchanged." << endl;
+            exchange(x, y);
+            cout << demo[i - 2] << " and " << demo[i-1] << " were sent for further sort." << endl;
+            //(IGNORE IF SOME RANDOM VALUE COMES WITH LEASE VALUE AS IT IS IN FRONT OF ARRAY AND IT DISPLAYS SOMETHING FROM INDEX i-1)
+            sortLeft(demo[i - 2], demo[i-1], i - 1);
+        }
+        else
+        {
+            cout << "-------------------- SORTING HALTED --------------------" << endl;
+            return;
+        }
+    }
+
+    void mainProcess()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            cout << "ITERATION " << i << endl;
+            if (demo[i - 1] > demo[i])
+            {
+                cout << "Needs to be exchanged" << endl;
+                cout << demo[i - 1] << " and " << demo[i] << " were sent to sort." << endl;
+                sortLeft(demo[i - 1], demo[i], i);
+            }
+            else{
+                cout<<"Iteration increased......"<<endl<<endl;
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            cout << demo[i] << ", ";
         }
     }
 };
 
 int main()
 {
-    Test t1;
-    t1.mainProcess();
+    InsertionSort i1;
+    i1.mainProcess();
     return 0;
 }
